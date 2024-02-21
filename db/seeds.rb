01daa2df9e0df3ddc2c53fd76cb463b3ad792ba7
Henrich Moraes <henrichm@gmail.com>
2024-02-21 10:26:26 -0300

    feat: add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+[
+  { id: 1, limite: 1_000_00, saldo: 0 },
+  { id: 2, limite: 800_00, saldo: 0 },
+  { id: 3, limite: 10_000_00, saldo: 0 },
+  { id: 4, limite: 100_000_00, saldo: 0 },
+  { id: 5, limite: 5_000_00, saldo: 0 }
+].each do |data|
+  Customer.create(data)
+end
